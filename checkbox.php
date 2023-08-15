45f6ffe95f15b4759423b75d8d5084b799220b07
monirbd <monirbd.me@gmail.com>
2023-08-16 00:34:37 +0600

    Array chckeced box with last comma removed by implode

--- checkbox.php
+++ checkbox.php
@@ -7,23 +7,39 @@
 
     <?php 
         if(isset($_POST['submit'])){
-           
+            if(isset($_POST['subject'])){
+                $selectSubjects = $_POST['subject'];
+                echo "Selected subjects: " . implode(", ", $selectSubjects);
+            }else{
+                echo "No Subject selected";
+            }
+
+            // $subjects = $_POST['subject'];
+            // echo "You have selected: ";
+            // $countSubject = count($subjects);
+            // foreach($subjects as $key=>$value){
+            //      echo $value;
+            //      if($key < $countSubject - 1){
+            //         echo ", ";
+            //      }
+            // }
         }
     ?>
 </head>
 <body>
-    <div id="subject"></div>
-    <form action="" method="post" name="allSubject" id="allSubject" onsubmit="selectSubject(); return false;">
+   
+    <form action="" method="post" name="allSubject" id="allSubject" onsubmit="checkedSubject(); return false;">
         <table>
             <tr>
                 <td><h1>Select Multiple Subject</h1></td>
             </tr>
             <tr>
                 <td>
-                    <input type="checkbox" name="subject[]" value="Bangla" <?php if(in_array("Bangla",$selectSubjects,true)) echo "checked"; ?> />Bangla
-                    <input type="checkbox" name="subject[]" value="English" <?php if(in_array("English",$selectSubjects,true)) echo "checked"; ?> />English
-                    <input type="checkbox" name="subject[]" value="Math" <?php if(in_array("Math",$selectSubjects,true)) echo "checked" ?> />Math
-                    <input type="checkbox" name="subject[]" value="Science" <?php if(in_array("Science",$selectSubjects,true)) echo "checked"?> />Science
+                    <input type="checkbox" name="subject[]" value="Bangla" />Bangla
+                    <input type="checkbox" name="subject[]" value="English"  />English
+                    <input type="checkbox" name="subject[]" value="Math"  />Math
+                    <input type="checkbox" name="subject[]" value="Science" />Science
+                    <input type="checkbox" name="subject[]" value="Other" />Other
                 </td>
             </tr>
             <tr>
